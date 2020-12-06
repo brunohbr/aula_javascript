3073c1dfc473b521799a426e0ac950d93e9bca8b
brunohbr <44102410+brunohbr@users.noreply.github.com>
2020-12-06 11:58:56 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,85 @@
+function botao(){
+    alert("obrigado por clicar");
+
+}
+
+
+
+
+
+/*
+function soma (n1, n2) {
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true;
+    }else{
+        validar =  false;
+    }
+     return validar;
+}
+var idade = prompt("Qual a sua idade");
+console.log(validaIdade(idade));
+*/
+//alert(soma (5,10));
+
+/*
+var d = new Date();
+alert(d);
+alert(d.getHours);
+*/
+
+/*
+var count;
+for ( count=0; count <= 5; count++){
+    alert(count);
+};
+
+*/
+
+/*
+var count = 0;
+while (count < 5) {
+    console.log(count);
+    count = count + 1;
+    
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade");
+if ( idade >= 18){
+    alert("maior idade");
+}else{
+    alert("menor idade");
+};
+*/
+
+
+
+//var fruta = {nome: "maçã", cor: "vermelha"};
+//console.log(fruta);
+
+
+//var lista = ["maça", "pera","laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+
+//var nome = "bruno";
+//var n1 = 5;
+//var n2 = 3;
+//var frase "Japão é o melhor time do mundo";
+//alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.tolowerCase());
+//alert(frase.replace("japão", "Brasil"));
